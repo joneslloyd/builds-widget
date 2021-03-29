c4acf023d2e4455656228755e2abd9f2e56c1d85
Lloyd Jones <lloyd@lloydjones.io>
2021-03-29 19:29:41 +0100

    Further loading tweaks

--- src/widget/app/components/more-builds/index.js
+++ src/widget/app/components/more-builds/index.js
@@ -6,6 +6,7 @@ import FlexRow from '../../styles/components/flex-row';
 import SmallWhiteText from '../../styles/components/small-white-text';
 
 const MoreBuildsStyles = tw(FlexRow)``;
+const MoreBuildsLoading = tw(SmallWhiteText)`ml-6`;
 const MoreBuildsSmallGoldText = tw(SmallWhiteText)`pl-6 flex items-center text-xs! font-medium text-widget-gold normal-case transition-all hover:text-widget-gold-light all:(transition-all fill-widget-gold) hover:(all:(fill-widget-gold-light))`;
 const MoreBuildsIconStyled = tw(MoreBuildsIcon)`ml-1`;
 
@@ -13,16 +14,21 @@ const MoreBuildsIconStyled = tw(MoreBuildsIcon)`ml-1`;
 const MoreBuilds = () => {
 
     const { squidexApiBuildData: { data: { championCommonInfo = [] } = {} } = {}, loading: isLoading = true } = useContext(BuildContext);
-    const { flatData: { name = '', slug = '' } = {} } = championCommonInfo[0] || {};
+    const { flatData: { name = 'xxxx', slug = '' } = {} } = championCommonInfo[0] || {};
 
     const moreBuildsLinkText = `See more ${name} builds`;
     const moreBuildsLinkHref = `https://app.mobalytics.gg/lol/champions/${slug}/build`;
 
-    const loading = isLoading || (!name || !slug);
+    const loading = isLoading || (!name || 'xxxx' === name);
 
     return (
         <MoreBuildsStyles>
-            <MoreBuildsSmallGoldText as={'a'} href={moreBuildsLinkHref} target="_blank" title={moreBuildsLinkText} loading={loading}>{moreBuildsLinkText}<MoreBuildsIconStyled /></MoreBuildsSmallGoldText>
+            {loading && (
+                <MoreBuildsLoading loading={true}>See more xxxxx builds...</MoreBuildsLoading>
+            )}
+            {!loading && (
+                <MoreBuildsSmallGoldText as={'a'} href={moreBuildsLinkHref} target="_blank" title={moreBuildsLinkText} loading={loading}>{moreBuildsLinkText}<MoreBuildsIconStyled /></MoreBuildsSmallGoldText>
+            )}
         </MoreBuildsStyles>
     );
 };
