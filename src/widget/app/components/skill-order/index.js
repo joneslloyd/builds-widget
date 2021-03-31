5f6d00013a004f3cf85f13ed524f0ce7494160ed
Lloyd Jones <lloyd@lloydjones.io>
2021-03-31 16:53:26 +0100

    Start skills order

--- src/widget/app/components/skill-order/index.js
+++ src/widget/app/components/skill-order/index.js
@@ -10,15 +10,22 @@ const SkillOrderSmallWhiteText = tw(SmallWhiteText)`uppercase`;
 
 const SkillOrder = () => {
 
-    const { dataApiBuildData: { data: { lol: { champion = {} } = {} } = {} } = {}, loading: isLoading = true } = useContext(BuildContext);
-    console.log(champion);
+    const { dataApiBuildData: { data: { lol: { champion: { build: { skillOrder = [] } = {} } = {} } = {} } = {} } = {}, squidexApiBuildData: { data: { championCommonInfo: [{ flatData: { abilities: abilitiesRaw = [] } = {} } = {}] = [] } = {} } = {}, loading: isLoading = true } = useContext(BuildContext);
+
+    const abilities = abilitiesRaw.filter(a => a.flatData.activationKey !== 'PASSIVE') || [];
+    const skillsMap = abilities.length > 0 ? abilities.reduce((a, i) => {
+        a.push(i.flatData.activationKey);
+        return a;
+    }, []) : false;
+
     const loading = isLoading || 1 === 1;
 
     return (
         <SkillOrderStyles>
             <SkillOrderSmallWhiteText>Skill order</SkillOrderSmallWhiteText>
             <FlexRow>
-                <p>TEST</p>
+                {/* <p>DEV – Skills map: {JSON.stringify(skillsMap)}</p>
+                <p>DEV - Skills order: {JSON.stringify(abilities)}</p> */}
             </FlexRow>
         </SkillOrderStyles>
     );
