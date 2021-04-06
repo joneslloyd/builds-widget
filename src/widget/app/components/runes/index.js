973ff3aa2a738be5d21fb46ff8900bed15d3603d
Lloyd Jones <lloyd@lloydjones.io>
2021-04-06 15:43:08 +0100

    ❗️TEST: Remove dynamic Runes loading (full/compact) and instead just import both. RESULT: 6kb larger file-size.
    Might reverse..

--- src/widget/app/components/runes/index.js
+++ src/widget/app/components/runes/index.js
@@ -1,36 +1,15 @@
-import { useState, useEffect, useRef } from 'preact/hooks';
+import CompactRunes from '../compact-runes';
+import FullRunes from '../full-runes';
 import { useContext } from 'preact/hooks';
 import { BuildContext } from '../../lib/context';
 
-const Runes = (props) => {
+const Runes = () => {
 
     const { layout = 'compact' } = useContext(BuildContext);
-    const RunesDisplayRef = useRef(null);
-    const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
+    const RunesComponent = 'compact' === layout ? (<CompactRunes />) : (<FullRunes />);
 
-        setLoading(true);
-
-        const importRunesDisplayComponent = async () => {
-            try {
-                const { default: runesDisplay } = await import(`../${layout}-runes/index.js`);
-                RunesDisplayRef.current = runesDisplay;
-            } catch (err) {
-                throw err;
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        importRunesDisplayComponent();
-
-    }, []);
-
-    if (!loading && RunesDisplayRef.current) {
-        const { current: ImportedRunesDisplayComponent } = RunesDisplayRef;
-        return <ImportedRunesDisplayComponent {...props} />;
-    }
+    return RunesComponent;
 };
 
 export default Runes;
