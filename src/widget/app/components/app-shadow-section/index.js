c12ac281227cf5cf3cf137f3f9e1896a51921909
Lloyd Jones <lloyd@lloydjones.io>
2021-04-20 10:41:01 +0100

    Correct Shadow DOM implementation

--- src/widget/app/components/app-shadow-section/index.js
+++ src/widget/app/components/app-shadow-section/index.js
@@ -1,13 +1,40 @@
 import register from 'preact-custom-element';
+import { useEffect, useRef } from 'preact/hooks';
+import LoadableAppWrapper from '../loadable-app-wrapper';
+import AppProvider from '../../lib/context'
+import * as goober from 'goober';
+import tw, { styled } from 'twin.macro';
 
-const AppShadowSection = ({ children }) => {
+/* eslint-disable no-undef */
+goober.setup(h);
+
+const AppShadowSectionStyles = styled('div')(() => [
+    tw`h-full w-full min-h-full max-w-screen-xl`,
+]);
+
+const AppShadowSection = (props) => {
+
+    const appShadowSectionRef = useRef(null);
+
+    useEffect(() => {
+
+        const currentShadowSection = appShadowSectionRef.current;
+
+        if (currentShadowSection) {
+
+            goober.css.call({ target: currentShadowSection.base }, goober.extractCss());
+        }
+    }, [appShadowSectionRef]);
+
     return (
-        <div>
-            {children}
-        </div>
+        <AppShadowSectionStyles ref={appShadowSectionRef}>
+            <AppProvider {...props}>
+                <LoadableAppWrapper />
+            </AppProvider>
+        </AppShadowSectionStyles>
     );
 };
 
-register(AppShadowSection, 'app-shadow-section', ['children'], { shadow: true });
+register(AppShadowSection, 'app-shadow-section', [], { shadow: true });
 
 export default AppShadowSection;
--- src/widget/index.js
+++ src/widget/index.js
@@ -1,9 +1,4 @@
 import habitat from 'preact-habitat';
-import { useEffect, useRef } from 'preact/hooks';
-import LoadableAppWrapper from './app/components/loadable-app-wrapper';
-import AppProvider from './app/lib/context'
-import * as goober from 'goober';
-import tw, { styled } from 'twin.macro';
 import './app/components/app-shadow-section';
 
 if (process.env.NODE_ENV === 'development') {
@@ -12,57 +7,10 @@ if (process.env.NODE_ENV === 'development') {
     require("preact/debug");
 }
 
-/* eslint-disable no-undef */
-goober.setup(h);
-
-const AppShadowSectionStyles = styled('app-shadow-section')(() => [
-    tw`h-full w-full min-h-full max-w-screen-xl`,
-]);
-
 const Widget = (props) => {
 
-    const appShadowSectionRef = useRef(null);
-
-    useEffect(() => {
-
-        const currentShadowSection = appShadowSectionRef.current;
-
-        if (currentShadowSection) {
-
-            console.log(currentShadowSection.base)
-
-            //Get style content from <head> style tag
-            const headStyleTag = document.querySelectorAll('head style#_goober')[0];
-
-            if (headStyleTag) {
-
-                let headStyles = headStyleTag.textContent;
-                //Remove <head> style tag
-                headStyleTag.parentNode.removeChild(headStyleTag);
-
-                //Create new <style> tag in shadow dom to prevent inheritance of parent styles
-                const newStyleP = document.createElement('style');
-                newStyleP.textContent = ':host{ all: initial !important; }';
-                currentShadowSection.base.appendChild(newStyleP);
-
-                //Create new <style> tag in shadow dom for Goober
-                const newStyle = document.createElement('style');
-                newStyle.id = '_goober';
-                newStyle.textContent = headStyles;
-                currentShadowSection.base.appendChild(newStyle);
-
-                const css = goober.css.bind({ target: currentShadowSection.base });
-                const styled = goober.styled.bind({ target: currentShadowSection.base });
-            }
-        }
-    }, [appShadowSectionRef]);
-
     return (
-        <AppShadowSectionStyles ref={appShadowSectionRef}>
-            <AppProvider {...props}>
-                <LoadableAppWrapper />
-            </AppProvider>
-        </AppShadowSectionStyles>
+        <app-shadow-section {...props} />
     );
 };
 
