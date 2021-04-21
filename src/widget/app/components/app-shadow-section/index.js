b750ce5b8838a7d260c476f6b2660e9dced26ceb
Lloyd Jones <lloyd@lloydjones.io>
2021-04-21 16:40:07 +0100

    Another attempt at fixing

--- src/widget/app/components/app-shadow-section/index.js
+++ src/widget/app/components/app-shadow-section/index.js
@@ -1,45 +1,27 @@
 import register from 'preact-custom-element';
-import { useEffect, useRef, useState } from 'preact/hooks';
+import { forwardRef } from 'preact/compat';
 import LoadableAppWrapper from '../loadable-app-wrapper';
 import AppProvider from '../../lib/context'
-import * as goober from 'goober';
-import tw, { styled } from 'twin.macro';
-import { globalStylesExport } from '../../styles/global-styles/global-styles-export';
-
+import tw, { styled, GlobalStyles } from 'twin.macro';
 
 const AppShadowSectionStyles = styled('div')(() => [
     tw`h-full w-full min-h-full max-w-screen-xl`,
 ]);
 
-const AppShadowSection = (props) => {
+const AppShadowSection = forwardRef((props, ref) => {
 
-    const appShadowSectionRef = useRef(null);
-    const [shadowCss, setShadowCss] = useState('');
-
-    useEffect(() => {
-        setTimeout(() => {
-            const currentShadowSection = appShadowSectionRef.current;
-
-            if (currentShadowSection) {
-
-                goober.styled.bind({ target: currentShadowSection.base });
-                goober.css.bind({ target: currentShadowSection.base });
-
-                setShadowCss(goober.extractCss() + globalStylesExport);
-            }
-        }, 1500);
-    }, [appShadowSectionRef]);
+    const { shadowCss } = props;
 
     return (
-        <AppShadowSectionStyles ref={appShadowSectionRef}>
+        <AppShadowSectionStyles ref={ref}>
             <style id={'_goober'} dangerouslySetInnerHTML={{ __html: ' ' + shadowCss }} />
             <AppProvider {...props}>
                 <LoadableAppWrapper />
             </AppProvider>
         </AppShadowSectionStyles>
     );
-};
+});
 
-register(AppShadowSection, 'app-shadow-section', [], { shadow: true });
+register(AppShadowSection, 'app-shadow-section', ['shadowCss'], { shadow: true });
 
 export default AppShadowSection;
--- src/widget/index.js
+++ src/widget/index.js
@@ -1,5 +1,8 @@
 import habitat from 'preact-habitat';
+import { createRef } from 'preact';
+import { useCallback, useEffect, useState } from 'preact/hooks';
 import * as goober from 'goober';
+import { globalStylesExport } from './app/styles/global-styles/global-styles-export';
 import './app/components/app-shadow-section';
 
 if (process.env.NODE_ENV === 'development') {
@@ -13,8 +16,36 @@ goober.setup(h);
 
 const Widget = (props) => {
 
+    const headStyles = document.head.querySelector('#_goober')?.firstChild?.textContent || false;
+    const appShadowSectionRef = createRef();
+    const [shadowCss, setShadowCss] = useState(false);
+
+    const doSetGoober = () => {
+
+        const currentShadowSection = appShadowSectionRef.current;
+
+        if (currentShadowSection) {
+
+            goober.styled.bind({ target: currentShadowSection });
+            goober.css.bind({ target: currentShadowSection });
+
+            const stylesToAdd = headStyles ? headStyles + globalStylesExport : globalStylesExport;
+
+            setShadowCss(stylesToAdd);
+
+        }
+    };
+
+    const setGoober = useCallback(() => {
+        doSetGoober();
+    }, [appShadowSectionRef, headStyles]);
+
+    useEffect(() => {
+        setGoober();
+    }, [appShadowSectionRef, headStyles]);
+
     return (
-        <app-shadow-section {...props} />
+        <app-shadow-section ref={appShadowSectionRef} shadowCss={shadowCss} {...props} />
     );
 };
 
