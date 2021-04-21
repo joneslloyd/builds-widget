ccf5e303d106fd625568ee8f9978cc6dc36c614d
Lloyd Jones <lloyd@lloydjones.io>
2021-04-21 16:50:47 +0100

    Remove Shadow DOM stuff for demo

--- src/widget/app/components/app-shadow-section/index.js
+++ src/widget/app/components/app-shadow-section/index.js
@@ -1,26 +1,22 @@
 import register from 'preact-custom-element';
-import { forwardRef } from 'preact/compat';
 import LoadableAppWrapper from '../loadable-app-wrapper';
 import AppProvider from '../../lib/context'
-import tw, { styled, GlobalStyles } from 'twin.macro';
+import tw, { styled } from 'twin.macro';
 
 const AppShadowSectionStyles = styled('div')(() => [
     tw`h-full w-full min-h-full max-w-screen-xl`,
 ]);
 
-const AppShadowSection = forwardRef((props, ref) => {
-
-    const { shadowCss } = props;
+const AppShadowSection = (props) => {
 
     return (
-        <AppShadowSectionStyles ref={ref}>
-            <style id={'_goober'} dangerouslySetInnerHTML={{ __html: ' ' + shadowCss }} />
+        <AppShadowSectionStyles>
             <AppProvider {...props}>
                 <LoadableAppWrapper />
             </AppProvider>
         </AppShadowSectionStyles>
     );
-});
+};
 
 register(AppShadowSection, 'app-shadow-section', ['shadowCss'], { shadow: true });
 
--- src/widget/index.js
+++ src/widget/index.js
@@ -3,7 +3,7 @@ import { createRef } from 'preact';
 import { useCallback, useEffect, useState } from 'preact/hooks';
 import * as goober from 'goober';
 import { globalStylesExport } from './app/styles/global-styles/global-styles-export';
-import './app/components/app-shadow-section';
+import AppShadowSection from './app/components/app-shadow-section';
 
 if (process.env.NODE_ENV === 'development') {
     // Must use require here as import statements are only allowed
@@ -16,36 +16,8 @@ goober.setup(h);
 
 const Widget = (props) => {
 
-    const headStyles = document.head.querySelector('#_goober')?.firstChild?.textContent || false;
-    const appShadowSectionRef = createRef();
-    const [shadowCss, setShadowCss] = useState(false);
-
-    const doSetGoober = () => {
-
-        const currentShadowSection = appShadowSectionRef.current;
-
-        if (currentShadowSection) {
-
-            goober.styled.bind({ target: currentShadowSection });
-            goober.css.bind({ target: currentShadowSection });
-
-            const stylesToAdd = headStyles ? headStyles + globalStylesExport : globalStylesExport;
-
-            setShadowCss(stylesToAdd);
-
-        }
-    };
-
-    const setGoober = useCallback(() => {
-        doSetGoober();
-    }, [appShadowSectionRef, headStyles]);
-
-    useEffect(() => {
-        setGoober();
-    }, [appShadowSectionRef, headStyles]);
-
     return (
-        <app-shadow-section ref={appShadowSectionRef} shadowCss={shadowCss} {...props} />
+        <AppShadowSection {...props} />
     );
 };
 
