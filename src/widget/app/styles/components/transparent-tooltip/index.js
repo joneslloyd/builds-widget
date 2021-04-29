460be4b65d64ef348cb096b25134539d41f4c18f
Lloyd Jones <lloyd@lloydjones.io>
2021-04-29 11:40:13 +0100

    Explicitly add font to tooltip as it wasn't inheriting due to tooltip parent node being changed

--- src/widget/app/styles/components/transparent-tooltip/index.js
+++ src/widget/app/styles/components/transparent-tooltip/index.js
@@ -1,3 +1,4 @@
+import tw from 'twin.macro';
 import TooltipWrapper from '../../../components/tooltips/tooltip-wrapper';
 import { useStyled } from '../../../lib/context/goober';
 import { parseStyles } from '../../../lib/helpers';
@@ -6,8 +7,10 @@ export const TransparentTooltip = ({ children, ...rest }) => {
 
     const styled = useStyled();
 
+    //Have to explicitly add 'font-sans' to the tooltip here because it's now rendered onto the shadow dom root node, which is outside of the existing declarations
     const TransparentTooltipC = styled(TooltipWrapper)(() => {
         return parseStyles([
+            tw`font-sans`,
             {
                 'background-color': 'transparent',
                 'border': 'solid 1px transparent',
